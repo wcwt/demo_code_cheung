3728d6771e824fb95f61945605f67f8a6a6f89af
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-11 16:01:44 +0800

    i

--- cheung/main.py
+++ cheung/main.py
@@ -4,6 +4,5 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-
 train_path = "../source_file/csci_data/SR-ARE-train/"
 test_path = "../source_file/csci_data/SR-ARE-test/"
