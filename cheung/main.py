6470126ab95b359fff67a0e9b91621d7423e21ce
wcwt <51042202+wcwt@users.noreply.github.com>
2020-11-11 15:50:04 +0800

    i

---
+++ cheung/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pickle
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+train_path = "../source_file/csci_data/SR-ARE-train/"
+test_path = "../source_file/csci_data/SR-ARE-test/"
